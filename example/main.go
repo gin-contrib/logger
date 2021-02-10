gin-contrib/logger

commit 64a5142f9be6e3865667f99933922c35be25dc99

docs: update example

example/main.go
@@ -50,22 +50,22 @@ func main() {
 
 	// Example ping request.
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Example skip path request.
 	r.GET("/skip", func(c *gin.Context) {
-		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Example skip path request.
 	r.GET("/regexp1", func(c *gin.Context) {
-		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Example skip path request.
 	r.GET("/regexp2", func(c *gin.Context) {
-		c.String(200, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Listen and Server in 0.0.0.0:8080

commit 128ee4c8d77e02870b4d1b42cfb1126ba55e0bcc

docs: fix typo in example
64a5142 accidentally introduced a typo / copy-paste error so the example no longer compiles

example/main.go
@@ -50,7 +50,7 @@ func main() {
 
 	// Example ping request.
 	r.GET("/ping", func(c *gin.Context) {
-		c.String(, "pong "+fmt.Sprint(time.Now().Unix()))
+		c.String(http.StatusOK, "pong "+fmt.Sprint(time.Now().Unix()))
 	})
 
 	// Example skip path request.