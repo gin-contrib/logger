0cf2a968f41fdd011bbf3bb03ec4b425a50cc48b
Bo-Yi Wu <appleboy.tw@gmail.com>
2023-07-09 10:30:42 +0800

    Revert "chore(logger): update logger from context after middleware execution" (#53)

--- logger.go
+++ logger.go
@@ -100,7 +100,6 @@ func SetLogger(opts ...Option) gin.HandlerFunc {
 			}
 			latency := end.Sub(start)
 
-			l = *zerolog.Ctx(c.Request.Context())
 			l = l.With().
 				Int("status", c.Writer.Status()).
 				Str("method", c.Request.Method).
